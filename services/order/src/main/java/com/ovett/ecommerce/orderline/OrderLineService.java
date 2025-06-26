24045c9c7757d44033b87943d91ca2b3cebb01aa
Ovett Mora <ovettmora17@gmail.com>
2025-06-25 21:39:04 -0600

    add OrderLineController, Service, Repository, and Response for order line management

--- services/order/src/main/java/com/ovett/ecommerce/orderline/OrderLineService.java
+++ services/order/src/main/java/com/ovett/ecommerce/orderline/OrderLineService.java
@@ -3,6 +3,8 @@ package com.ovett.ecommerce.orderline;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class OrderLineService {
@@ -12,5 +14,12 @@ public class OrderLineService {
     public Integer saveOrderLine(OrderLineRequest request) {
         var order = orderLineMapper.toOrderLine(request);
         return orderLineRepository.save(order).getId();
+    }
+
+    public List<OrderLineResponse> findByOrderId(Integer orderId) {
+        return orderLineRepository.findAllByOrderId(orderId)
+                .stream()
+                .map(orderLineMapper::toOrderLineResponse)
+                .toList();
     }
 }
